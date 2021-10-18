d312c2a4bbb5ab2a1154b28c278c3c7c6ecface1
well300 <90952986+well300@users.noreply.github.com>
2021-10-18 14:33:16 +0530

    Create Rin.ts

---
+++ src/commands/Misc/Rin.ts
@@ -0,0 +1,47 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { join } from 'path'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'rin',
+            description: 'Displays the info.',
+            category: 'misc',
+            usage: `${client.config.prefix}rin`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        const n = [
+            './assets/videos/Rin/rin-git.mp4'
+        ]
+        let rin = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: rin }, MessageType.video, {quoted:M.WAMessage,
+            mimetype: Mimetype.gif,
+            caption: `🎗 Rin\n\n⚜ *Description: well...\n\n` }
+        )
+    }
+}
+          
+       
+
+
+    
+        
+           
+           
+            
+            
+        
+    
+
+    
+        
+           
+           
+           
+   
