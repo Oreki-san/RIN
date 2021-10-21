ad7be5f25ad1c6a5c7be29e64a28a690aa9ade22
JJ <77143046+Oreki-san@users.noreply.github.com>
2021-10-21 20:09:44 +0530

    Create define.ts

---
+++ src/commands/eductive/define.ts
@@ -0,0 +1,71 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+
+import BaseCommand from '../../lib/BaseCommand'
+
+import WAClient from '../../lib/WAClient'
+
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+
+import axios from 'axios'
+
+import request from '../../lib/request'
+
+import { MessageType } from '@adiwajshing/baileys'
+
+export default class Command extends BaseCommand {
+
+    constructor(client: WAClient, handler: MessageHandler) {
+
+        super(client, handler, {
+
+            command: 'define',
+
+            aliases: ['d'],
+
+            description: 'Gives you the defination of the given word. ',
+
+            category: 'educative',
+
+            usage: `${client.config.prefix}define [Word you want to search about]`,
+
+            baseXp: 50
+
+        })
+
+    }
+
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+
+        if (!joined) return void M.reply('Please provide a word .')
+
+        const term = joined.trim()
+
+        console.log(term,joined)
+
+        await axios
+
+            .get(
+
+                `http://api.urbandictionary.com/v0/define?term=${term}`
+
+            )
+
+           .then((response) => {
+
+                // console.log(response);
+
+                const text = `📚 Defination of *${term}* is\n\n📖 *Defination :* ${response.data.list[0].definition.replace(/\[/g,'').replace(/\]/g,'')}\n\n💬 *Example :* ${response.data.list[0].example.replace(/\[/g,'').replace(/\]/g,'')}`
+
+          M.reply(text)
+
+                })
+
+            .catch((err) => {
+
+                M.reply(`Sorry, couldn't find any definations related to *${term}*.`)
+
+            })
+
+    }
+
+}
