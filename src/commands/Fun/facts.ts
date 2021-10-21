49aaaa4c876aa208dc5000a91f69ae1e86c6f72d
JJ <77143046+Oreki-san@users.noreply.github.com>
2021-10-21 20:21:31 +0530

    Create facts.ts

---
+++ src/commands/Fun/facts.ts
@@ -0,0 +1,57 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+
+import BaseCommand from '../../lib/BaseCommand'
+
+import WAClient from '../../lib/WAClient'
+
+import { ISimplifiedMessage } from '../../typings'
+
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+
+    constructor(client: WAClient, handler: MessageHandler) {
+
+        super(client, handler, {
+
+            command: 'fact',
+
+            description: 'sends a random fact for you.',
+
+            aliases: ['facts'],
+
+            category: 'fun',
+
+            usage: `${client.config.prefix}fact`,
+
+            baseXp: 30
+
+        })
+
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+
+        await axios
+
+            .get(`https://nekos.life/api/v2/fact`)
+
+            .then((response) => {
+
+                // console.log(response);
+
+                const text = `📝 *Fact:* ${response.data.fact}`
+
+                M.reply(text)
+
+            })
+
+            .catch((err) => {
+
+                M.reply(`🔍 Error: ${err}`)
+
+            })
+
+    }
+
+}
