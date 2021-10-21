63f93083f494e3d99ec54d767ea9c54f60f9b7b4
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-10-21 13:29:19 +0530

    Update do-you-mean.ts

--- src/commands/General/do-you-mean.ts
+++ src/commands/General/do-you-mean.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            './assets/videos/Rin/rin-smile.mp4'
+            './assets/Rin/rin-smile.mp4'
         ]
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.video, {quoted:M.WAMessage,
