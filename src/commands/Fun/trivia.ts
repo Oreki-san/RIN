bd40b4be441f5d9d3e4a3a8fdc3626f8fdd03799
JJ <77143046+Oreki-san@users.noreply.github.com>
2021-10-25 12:49:30 +0530

    Create trivia.ts

---
+++ src/commands/Fun/trivia.ts
@@ -0,0 +1,29 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'trivia',
+            description: 'random trivia.',
+            aliases: ['trivia'],
+            category: 'fun',
+            usage: `${client.config.prefix}trivia`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        await axios.get(`http://numbersapi.com/random/trivia`)
+        .then((response) => {
+                // console.log(response);
+                const text = `📝 *Trivia:* ${response.data}`
+                M.reply(text);
+            }).catch(err => {
+                M.reply(`🔍 Error: ${err}`)
+            }
+            )
+    };
+}
