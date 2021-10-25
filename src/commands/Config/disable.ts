9390e32002982f9ada189acdecd470f7f7cd684f
JJ <77143046+Oreki-san@users.noreply.github.com>
2021-10-26 01:55:12 +0530

    Update disable.ts

--- src/commands/Config/disable.ts
+++ src/commands/Config/disable.ts
@@ -8,7 +8,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'disable',
             description: 'Disables the given command from being used globally',
-            category: 'config',
+            category: 'Config',
             dm: true,
             usage: `${client.config.prefix}config [command] | (reason)`,
             modsOnly: true,
