651777b41943d57769e40af144a41924fc42276b
Shin Nouzen <85594230+ShinNouzen@users.noreply.github.com>
2021-11-05 12:19:28 +0530

    Update nsfwneko.ts

--- src/commands/NSFW/nsfwneko.ts
+++ src/commands/NSFW/nsfwneko.ts
@@ -21,11 +21,15 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         // fetch result of https://api.waifu.pics/nsfw/neko from the API using axios
         const { data } = await axios.get('https://api.waifu.pics/nsfw/neko')
-        const buffer = await request.buffer(data.url).catch((e) => {
-           if ( !(await this.client.getGroupData(M.from)).nsfw)
+        if ( !(await this.client.getGroupData(M.from)).nsfw)
             return void M.reply(
                 `Cannot Display NSFW content before enabling. Use ${this.client.config.prefix}activate nsfw to activate nsfw`
             )
+        const buffer = await request.buffer(data.url).catch((e) => {
+          // if ( !(await this.client.getGroupData(M.from)).nsfw)
+           // return void M.reply(
+                //`Cannot Display NSFW content before enabling. Use ${this.client.config.prefix}activate nsfw to activate nsfw`
+            //)
             return void M.reply(e.message)
         })
         while (true) {
