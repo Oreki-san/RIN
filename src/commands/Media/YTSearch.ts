d02bf01796421a1ea154392480819eb2d5a9ccba
JJ <77143046+Oreki-san@users.noreply.github.com>
2021-11-09 01:24:38 +0530

    Update YTSearch.ts

--- src/commands/Media/YTSearch.ts
+++ src/commands/Media/YTSearch.ts
@@ -8,39 +8,41 @@ import yts from 'yt-search'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'yts',
+            command: 'ytsearch',
             description: 'Searches YT',
             category: 'media',
-            aliases: ['ytsearch'],
+            aliases: ['yts'],
             usage: `${client.config.prefix}yts [term]`,
-            dm: true,
             baseXp: 20
         })
     }
 
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        if (!joined) return void M.reply('Please provide a search term')
+        if (!joined) return void M.reply('🔎 Provide a search term')
         const term = joined.trim()
         const { videos } = await yts(term)
-        if (!videos || videos.length <= 0) return void M.reply(`No Matching videos found for the term *${term}*`)
+        if (!videos || videos.length <= 0) return void M.reply(`⚓ No Matching videos found for : *${term}*`)
         const length = videos.length < 10 ? videos.length : 10
-        let text = `🔎 *Results*\n\n`
+        let text = `🔎 *Results for ${term}*\n`
         for (let i = 0; i < length; i++) {
             text += `*#${i + 1}*\n📗 *Title:* ${videos[i].title}\n📕 *Channel:* ${
                 videos[i].author.name
             }\n 📙 *Duration:* ${videos[i].duration}\n📘 *URL:* ${videos[i].url}\n\n`
         }
-        this.client.sendMessage(M.from, text, MessageType.extendedText, {
-            quoted: M.WAMessage,
-            contextInfo: {
-                externalAdReply: {
-                    title: `Search Term: ${term}`,
-                    body: `🔰 ${this.client.config.name} 🔰`,
-                    mediaType: 2,
-                    thumbnailUrl: videos[0].thumbnail,
-                    mediaUrl: videos[0].url
+        M.reply('👾 searching...')
+        this.client
+            .sendMessage(M.from, text, MessageType.extendedText, {
+                quoted: M.WAMessage,
+                contextInfo: {
+                    externalAdReply: {
+                        title: `Search Term: ${term}`,
+                        body: `👾Handcrafted for you by Kaoi👾`,
+                        mediaType: 2,
+                        thumbnailUrl: videos[0].thumbnail,
+                        mediaUrl: videos[0].url
+                    }
                 }
-            }
-        })
+            })
+            .catch((reason: any) => M.reply(`❌ an error occurred, Reason: ${reason}`))
     }
 }
