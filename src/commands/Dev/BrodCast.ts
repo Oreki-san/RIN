ffd7d6a5a64109e96521ef7dc397ebb393c424cc
JJ <77143046+Oreki-san@users.noreply.github.com>
2021-11-14 17:14:28 +0530

    Update BrodCast.ts

--- src/commands/Dev/BrodCast.ts
+++ src/commands/Dev/BrodCast.ts
@@ -22,7 +22,7 @@ run = async (M: ISimplifiedMessage,  { joined }: IParsedArgs): Promise<void> => 
         const term = joined.trim();
         const chats:any= this.client.chats.all().filter(v => !v.read_only && !v.archive).map(v => v.jid).map(jids => jids.includes("g.us")? jids : null).filter(v=>v);
         for(let i =0;i<chats.length;i++){
-        const text = `*「 Kaoi Broadcast 」* \n *📢 announcement :* \n${term} By *${M.sender.username}*`
+        const text = `*「 📣RIN Broadcast📣 」* \n *📢 announcement :* \n${term} By *${M.sender.username}*`
         this.client.sendMessage(chats[i], text,MessageType.text,{contextInfo : {mentionedJid : M.groupMetadata?.participants.map((user) => user.jid) }})
         }
     }
