57f6dabef6c2a9c8fa7ff67b14f08fb74605d295
JJ <77143046+Oreki-san@users.noreply.github.com>
2021-11-20 21:37:49 +0530

    Update wallpaper.ts

--- src/commands/Weeb/wallpaper.ts
+++ src/commands/Weeb/wallpaper.ts
@@ -1,4 +1,4 @@
-import { AnimeWallpaper } from "anime-wallpapers";
+import { AnimeWallpaper } from "anime-wallpaper";
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
