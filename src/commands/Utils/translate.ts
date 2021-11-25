ef749a71c11621df574e3bdbe9274046088fa07d
JJ <77143046+Oreki-san@users.noreply.github.com>
2021-11-25 19:34:46 +0530

    Update translate.ts

--- src/commands/Utils/translate.ts
+++ src/commands/Utils/translate.ts
@@ -1,29 +1,38 @@
-import translate from 'translate-google'
-import MessageHandler from '../../Handlers/MessageHandler'
-import BaseCommand from '../../lib/BaseCommand'
-import WAClient from '../../lib/WAClient'
-import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+import translate from "translate-google";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
 
 export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'translate',
-            aliases: ['tr'],
-            description: 'Will translate the given word to your selected language. ',
-            category: 'utils',
-            usage: `${client.config.prefix}tr <code of the language that you want>|<word>\n\nExample: ${client.config.prefix}tr zh-cn|Hello`,
-            baseXp: 40
-        })
-    }
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "translate",
+			aliases: ["tr"],
+			description: "Will translate the given word to your selected language. ",
+			category: "utils",
+			usage: `${client.config.prefix}tr <word>|<language_code>\n\nExample: ${client.config.prefix}tr zh-cn|Hello`,
+			baseXp: 40,
+		});
+	}
 
-    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        const w = joined.trim().split('|')
-        if (w[0] === '') return void M.reply(`Use ${this.client.config.prefix}tr (language code|word)`)
-        const l = w[0]
-        const t = w[1]
-        if (!t) return void M.reply('Give me the word to translate!')
-        const q = await translate(t, { to: l })
-        const text = `${q}`
-        M.reply(text)
-    }
+	run = async (
+		M: ISimplifiedMessage,
+		{ joined }: IParsedArgs
+	): Promise<void> => {
+		const texts = joined.trim().split("|");
+		if (texts[0] === "")
+			return void M.reply(
+				`Use ${this.client.config.prefix}tr (word_that_you_wanna_translate|language_code)`
+			);
+		const word = texts[0];
+		const code = texts[1];
+		if (!code) return void M.reply("Give me the language code, Baka!");
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const response = await translate(word, { to: code }).catch((err: any) => {
+			return void M.reply(`Invalid language code, Baka!`);
+		});
+		const text = `${response}`;
+		M.reply(text);
+	};
 }
