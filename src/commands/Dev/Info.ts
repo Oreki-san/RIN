493602c719897440b021094a279101f468f2f348
JJ <77143046+Oreki-san@users.noreply.github.com>
2021-11-26 20:07:30 +0530

    Create Info.ts

---
+++ src/commands/Dev/Info.ts
@@ -0,0 +1,39 @@
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "info",
+			description: "Will display the info the bot",
+			category: "dev",
+			usage: `${client.config.prefix}info`,
+			baseXp: 0,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		/*eslint-disable @typescript-eslint/no-explicit-any*/
+		const chats: any = this.client.chats
+			.all()
+			.filter((v) => !v.read_only && !v.archive)
+			.map((v) => v.jid)
+			.map((jids) => (jids.includes("g.us") ? jids : null))
+			.filter((v) => v);
+		const pad = (s: any) => (s < 10 ? "0" : "") + s;
+		const formatTime = (seconds: any) => {
+			const hours = Math.floor(seconds / (60 * 60));
+			const minutes = Math.floor((seconds % (60 * 60)) / 60);
+			const secs = Math.floor(seconds % 60);
+			return `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
+		};
+		const uptime = () => formatTime(process.uptime());
+		await M.reply(
+			`*━━━❰ 🤍✨RIN✨🤍 ❱━━━*\n\n🔮 *Groups: ${
+		this.client.chats.all().filter(chat => chat.jid.endsWith('g.us')).length
+			}*\n\n🚦 *Uptime:${uptime()}*`
+		);
+	};
+}
