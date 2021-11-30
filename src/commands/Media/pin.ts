814fac5833a6e1f4769ce788b805186005aed0b7
JJ <77143046+Oreki-san@users.noreply.github.com>
2021-11-30 12:47:44 +0530

    Create pin.ts

---
+++ src/commands/Media/pin.ts
@@ -0,0 +1,54 @@
+import { MessageType } from '@adiwajshing/baileys'
+import request from '../../lib/request'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'pinterest',
+            aliases: ['pi', 'pin'],
+            description: 'Search wallpaper from pinterest.com. ',
+            category: 'media',
+            dm: true,
+            usage: `${client.config.prefix}pinterest [name]`
+        })
+    }
+    // static count = 0
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        
+        if (!joined) return void M.reply('Provide the keywords you wanna search, Baka!')
+        const chitoge = joined.trim()
+        console.log(chitoge)
+        const { data } = await axios.get(`https://api.zekais.com/pinterest?query=${chitoge}&apikey=CnXf9Ojs`)
+        if ((data as { error: string }).error) return void (await M.reply('Sorry, couldn\'t find'))
+        const buffer = await request.buffer(data.result[Math.floor(Math.random() * data.result.length)]).catch((e) => {
+            return void M.reply(e.message)
+        })
+        while (true) {
+            try {
+                M.reply(
+                    buffer || '🤍 An error occurred. Please try again later',
+                    MessageType.image,
+                    undefined,
+                    undefined,
+                    `💐 *Result: ${chitoge} has been found*\n`,
+                    undefined
+                ).catch((e) => {
+                    console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
+                    // console.log('Failed')
+                    M.reply(`🤍An error occurred. Please try again later.`)
+                })
+                break
+            } catch (e) {
+                // console.log('Failed2')
+                M.reply(`âœ– An error occurred. Please try again later.`)
+                console.log(`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
+            }
+        }
+        return void null
+    }
+}
